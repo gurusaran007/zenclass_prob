b8bf2cf8997a623d14f4c1bb6380629e69b89694
gurusaran007 <55083014+gurusaran007@users.noreply.github.com>
2023-12-17 12:20:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function foo(){
+    var res = document.getElementById("text1").value;
+    console.log(res);
+    var res1 = document.getElementById("text2").value;
+    console.log(res1);
+    var res2 = document.getElementById("text3").value;
+    console.log(res2);
+    var em = document.getElementById("email").value;
+    console.log(em);
+}
